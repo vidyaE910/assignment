6907df7305382f2cc70539ccfd40edf91cdeb693
vidyaE910 <pavankumar.emmadi@allianz.com>
2016-11-08 20:24:00 +0530

    Update myAssignment.java

--- myAssignment.java
+++ myAssignment.java
@@ -1,29 +1,29 @@
 //This is the java program to implement the knapsack problem using Dynamic Programming
 import java.util.Scanner;
-public class Knapsack_DP 
+public class Assignment 
 {
-    static int max(int a, int b) 
+    static int max(int r, int s) 
         { 
-            return (a > b)? a : b; 
+            return (r > s)? r : s; 
         }
-        static int knapSack(int W, int wt[], int val[], int n)
+        static int knapSack(int t, int y[], int x[], int n)
         {
             int i, w;
-            int [][]K = new int[n+1][W+1];
+            int [][]K = new int[n+1][t+1];
     	   // Build table K[][] in bottom up manner
             for (i = 0; i <= n; i++)
             {
-                for (w = 0; w <= W; w++)
+                for (w = 0; w <= t; w++)
                 {
                     if (i==0 || w==0)
                         K[i][w] = 0;
-                    else if (wt[i-1] <= w)
-                        K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w]);
+                    else if (y[i-1] <= w)
+                        K[i][w] = max(x[i-1] + K[i-1][w-y[i-1]],  K[i-1][w]);
                     else
                         K[i][w] = K[i-1][w];
                 }
             }
-            return K[n][W];
+            return K[n][t];
         }
         public static void main(String args[])
         {
@@ -32,20 +32,20 @@ public class Knapsack_DP
             //int n = sc.nextInt();
 			int n = 101;
             System.out.println("Enter the items weights: ");
-            int []wt = new int[n];
+            int []y = new int[n];
             for(int i=0; i<n; i++)
-                wt[i] = sc.nextInt();
+                y[i] = sc.nextInt();
             System.out.println("Enter the items values: ");
-            int []val = new int[n];
+            int []x = new int[n];
             for(int i=0; i<n; i++)
-                val[i] = sc.nextInt();
+                x[i] = sc.nextInt();
             //System.out.println("Enter the maximum capacity: ");
             //int W = sc.nextInt();
-			int W = 10000;
+			int t = 10000;
 			System.out.println("The number of items: " + n);
-			System.out.println("The Maximum Capacity: " + W);
+			System.out.println("The Maximum Capacity: " + t);
 			System.out.println(" ");
-            System.out.println("The maximum value that can be put in a knapsack of capacity W is: " + knapSack(W, wt, val, n));
+            System.out.println("The maximum value that can be put in a knapsack of capacity W is: " + knapSack(t, y, x, n));
             sc.close();
         }
 }
